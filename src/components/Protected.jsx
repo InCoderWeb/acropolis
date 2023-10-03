17e8829451639878be53121d5d6aa3e4a53bdf98
Ashutosh Tiwari <97720235+InCoderWeb@users.noreply.github.com>
2023-10-04 00:06:53 +0530

    latest update

---
+++ src/components/Protected.jsx
@@ -0,0 +1,20 @@
+"use client"
+import { useSession } from "next-auth/react"
+import { redirect, usePathname } from "next/navigation"
+
+const Protected = ({children}) => {
+    const { status } = useSession();
+    const path = usePathname();
+    console.log(status);
+    if(status == "authenticated"){
+        return <>{children}</>;
+    } else if(status == "unauthenticated"){
+      if(path != "/signin" && path != "/signup"){
+        redirect("/signin")
+      } else{
+        return <>{children}</>;
+      }
+    }
+}
+
+export default Protected
